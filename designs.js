ef7de195b250f73a7b2980941bb47e12c5feabe2
Joanna M <55323877+Jo-llama@users.noreply.github.com>
2020-09-28 19:58:52 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -37,12 +37,13 @@ However it doesn't work perfectly.
 Once the cell is colored, the clicking doesn't undo the color, 
 but if I pick another color from the picker after double click the cell color changes:
 
-function makeGrid (h, w) {matrix.innerHTML = ""; 
+function makeGrid(h, w) {
+    matrix.innerHTML = "";
     for (let x = 0; x < h; x++) {
-        let row = matrix.insertRow(x);
+        let tr = matrix.insertRow(x);  //storing rows in variable
         for (let y = 0; y < w; y++) {
-            let cell = row.insertCell(y);
-            cell.addEventListener("click", function(evt) {
+            let td = tr.insertCell(y); //storing columns in variable
+            td.addEventListener("click", function(evt) {
                 let classes = evt.target.classList;
                 let result = classes.toggle("style");
                 if (result) {
