3ceb74d7d2e4850d7979dbe0082ef625637916e9
Joanna M <55323877+Jo-llama@users.noreply.github.com>
2020-09-28 19:40:24 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,5 +1,5 @@
 //Select color input
-const color = document.getElementById("colorPicker");
+const color = document.getElementById("colorPicker")
 //Select size input
 const matrix = document.getElementById("pixelCanvas");
 const size = document.getElementById("sizePicker");
@@ -7,8 +7,8 @@ const size = document.getElementById("sizePicker");
 //Submitting grid size
 size.addEventListener("submit", function(evt) {
     evt.preventDefault();
-    const h = document.getElementById("inputHeight").value; //height element
-    const w = document.getElementById("inputWidth").value;  //width element
+    const h = document.getElementById("inputHeight").value;
+    const w = document.getElementById("inputWidth").value;
     makeGrid(h, w);
     
 });
@@ -17,10 +17,10 @@ size.addEventListener("submit", function(evt) {
  function makeGrid(h, w) {
     matrix.innerHTML = "";
     for (let x = 0; x < h; x++) {
-        let row = matrix.insertRow(x);
+        let tr = matrix.insertRow(x);  //storing rows in variable
         for (let y = 0; y < w; y++) {
-            let cell = row.insertCell(y);
-            cell.addEventListener("click", function(evt) {
+            let td = tr.insertCell(y); //storing columns in variable
+            td.addEventListener("click", function(evt) {
                 if (evt.target.hasAttribute("style")) {
                     evt.target.removeAttribute("style");
                 } 
@@ -37,17 +37,19 @@ However it doesn't work perfectly.
 Once the cell is colored, the clicking doesn't undo the color, 
 but if I pick another color from the picker after double click the cell color changes:
 
-function makeGrid (h, w) {matrix.innerHTML = ""; 
+function makeGrid(h, w) {
+    matrix.innerHTML = "";
     for (let x = 0; x < h; x++) {
-        let row = matrix.insertRow(x);
+        let tr = matrix.insertRow(x);  //storing rows in variable
         for (let y = 0; y < w; y++) {
-            let cell = row.insertCell(y);
-            cell.addEventListener("click", function(evt) {
+            let td = tr.insertCell(y); //storing columns in variable
+            td.addEventListener("click", function(evt) {
                 let classes = evt.target.classList;
                 let result = classes.toggle("style");
                 if (result) {
                     evt.target.style.backgroundColor = color.value;
                 }
+                else
             });
         }
     }
