525c5f57a961c5222cf3c34ee51ace2f781bad51
Joanna M <55323877+Jo-llama@users.noreply.github.com>
2020-09-28 18:46:38 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,30 +1,54 @@
-
 //Select color input
-const color = document.getElementById("colorPicker")
+const color = document.getElementById("colorPicker");
 //Select size input
 const matrix = document.getElementById("pixelCanvas");
 const size = document.getElementById("sizePicker");
 
-size.addEventListener("submit", function(event) {
-    event.preventDefault();
-    const height = document.getElementById("inputHeight").value;
-    const width = document.getElementById("inputWidth").value;
-    makeGrid(height, width);
+//submit grid size
+size.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+    const h = document.getElementById("inputHeight").value; //height element
+    const w = document.getElementById("inputWidth").value;  //width element
+    makeGrid(h, w);
     
 });
+
 // When size is submitted by the user, call makeGrid()
-function makeGrid(height, width) {matrix.innerHTML = "";
-    for (let x = 0; x < height; x++) {
-        var row = matrix.insertRow(x);
-        for (let y = 0; y < width; y++) {
-            var cell = row.insertCell(y);
-            cell.addEventListener("click", function(event) {
-                if (event.target.hasAttribute("style")) {
-                    event.target.removeAttribute("style");
-                } else {
-                    event.target.style.backgroundColor = color.value;
+ function makeGrid(h, w) {
+    matrix.innerHTML = "";
+    for (let x = 0; x < h; x++) {
+        let row = matrix.insertRow(x);
+        for (let y = 0; y < w; y++) {
+            let cell = row.insertCell(y);
+            cell.addEventListener("click", function(evt) {
+                if (evt.target.hasAttribute("style")) {
+                    evt.target.removeAttribute("style");
+                } 
+                else { 
+                    evt.target.style.backgroundColor = color.value;
                 }
             });
         }
     }
 }
+
+/* I also tried to use the toggle method, rather than if-else statement. 
+However it doesn't work perfectly.
+Once the cell is colored, the clicking doesn't undo the color, 
+but if I pick another color from the picker after double click the cell color changes:
+
+function makeGrid (h, w) {matrix.innerHTML = ""; 
+    for (let x = 0; x < h; x++) {
+        let row = matrix.insertRow(x);
+        for (let y = 0; y < w; y++) {
+            let cell = row.insertCell(y);
+            cell.addEventListener("click", function(evt) {
+                let classes = evt.target.classList;
+                let result = classes.toggle("style");
+                if (result) {
+                    evt.target.style.backgroundColor = color.value;
+                }
+            });
+        }
+    }
+}*/
